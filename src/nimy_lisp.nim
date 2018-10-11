f15f87650e89b4b3ca0aff8ff0a8665d525d4b94
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-11 10:47:30 -0400

    The user might more likely change sep than op; so move op to last

--- src/nimy_lisp.nim
+++ src/nimy_lisp.nim
@@ -3,18 +3,20 @@ import sequtils
 proc dollar[T](s: T): string =
   result = $s
 
-proc mapconcat*[T](s: openArray[T]; op: proc(x: T): string = dollar; sep = " "): string =
+proc mapconcat*[T](s: openArray[T]; sep = " "; op: proc(x: T): string = dollar): string =
   ## Concatenate elements of ``s`` after applying ``op`` to each element.
   ## Separate each element using ``sep``.
   for x in s:
     result.add op(x) & sep
 
 when isMainModule:
+  import strformat
   let
     s1 = @["abc", "def", "ghi"]
     s2 = ["abc", "def", "ghi"]
     s3 = [1, 2, 3]
-  echo s1.mapconcat()
-  echo s2.mapconcat()
-  echo s3.mapconcat()
-  echo s3.mapconcat(proc(x: int): string = "Ha: " & $x, sep = "\n")
+  echo fmt"`{s1.mapconcat()}'"
+  echo fmt"`{s2.mapconcat()}'"
+  echo fmt"`{s3.mapconcat()}'"
+  let foo = s3.mapconcat("\n", proc(x: int): string = "Ha: " & $x)
+  echo fmt"`{foo}'"
