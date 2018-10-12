6f5229e8bebf4017217c48cbbcdc498187af2575
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-12 15:23:31 -0400

    Add member proc

--- src/nimy_lisp.nim
+++ src/nimy_lisp.nim
@@ -11,6 +11,14 @@ proc mapconcat*[T](s: openArray[T]; sep = " "; op: proc(x: T): string = dollar):
     if i < s.high:
       result.add(sep)
 
+proc member*[T](el: T; s: openArray[T]): bool =
+  ## Return ``true`` if ``el`` is an element in ``s``.
+  ## This proc is like ``find`` but with args in reverse order, and
+  ## returns a bool instead.
+  result = false
+  if s.find(el) >= 0:
+    result = true
+
 when isMainModule:
   let
     s1 = @["abc", "def", "ghi"]
@@ -23,5 +31,13 @@ when isMainModule:
   doAssert s3.mapconcat() == "1 2 3"
   doAssert s3.mapconcat("\n", proc(x: int): string = "Ha: " & $x) == "Ha: 1\nHa: 2\nHa: 3"
   doAssert s4.mapconcat() == ""
+
+  doAssert "abc".member(s1) == true
+  doAssert "abc".member(s2) == true
+  doAssert "a".member(s2) == false
+  doAssert 1.member(s3) == true
+  doAssert 100.member(s3) == false
+  doAssert "".member(s4) == false
+  doAssert "a".member(s4) == false
 
   echo "\nTests passed!"
