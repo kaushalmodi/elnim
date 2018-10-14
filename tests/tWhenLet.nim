4e1730a3deeadbd5c7be6761342155541d734f97
Vindaar <basti90@gmail.com>
2018-10-14 12:22:47 +0200

    add a few more `when_let` test cases, including "nil like"

--- tests/tWhenLet.nim
+++ tests/tWhenLet.nim
@@ -33,3 +33,47 @@ suite "[when-let]":
           result = x.float
         res = "y as float / 10.0 = " & $(test(y) / 10.0)
     check res == "y as float / 10.0 = 12.5"
+
+  test "[when-let] false by bool":
+    var res = ""
+    when_let:
+      a = 5
+      b = a * 5
+      c = false
+      op:
+        res = "Hallo! " & $a & " and " & $b
+    check res == ""
+
+  test "[when-let] false from proc call":
+    proc callFalse(x: int): bool =
+      result = if x < 10: false else: true
+    var res = ""
+    when_let:
+      a = 5
+      b = a * 5
+      c = callFalse(a)
+      op:
+        res = "a is smaller 10!"
+    check res == ""
+
+  test "[when-let] assigning a seq":
+    var res: seq[int] = @[]
+    when_let:
+      a = @[1, 2, 3]
+      b = 5
+      op:
+        for x in a:
+          res.add x * b
+    check res == @[5, 10, 15]
+
+  test "[when-let] false from emtpy seq":
+    proc emptySeq(x: int): seq[int] =
+      for i in 0 ..< x:
+        result.add i
+    var res = ""
+    when_let:
+      a = 0
+      b = emptySeq(a)
+      op:
+        res = "Seq is empty!"
+    check res == ""
