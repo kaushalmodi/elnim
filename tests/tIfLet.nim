b5c2c92b6f2178085acad4404daa57ce021249c7
Vindaar <basti90@gmail.com>
2018-10-15 12:09:35 +0200

    change tests to `tIfLet` and add tests for `false` branch

--- tests/tIfLet.nim
+++ tests/tIfLet.nim
@@ -1,20 +1,20 @@
 import nimy_lisp
 import unittest
 
-suite "[when-let]":
+suite "[if-let]":
 
-  test "[when-let] simple assignments":
+  test "[if-let] simple assignments":
     var res = ""
-    when_let:
+    if_let:
       n = 5
       m = n * 5
       op:
         res = "Hallo! " & $n & " and " & $m
     check res == "Hallo! 5 and 25"
 
-  test "[when-let] assignment with procedure":
+  test "[if-let] assignment with procedure":
     var res = ""
-    when_let:
+    if_let:
       a = 5
       b = a * 5
       c = proc(x, y: int): int = x + y
@@ -22,9 +22,9 @@ suite "[when-let]":
         res = "Output is: " & $c(a, b)
     check res == "Output is: 30"
 
-  test "[when-let] multiple line body":
+  test "[if-let] multiple line body":
     var res = ""
-    when_let:
+    if_let:
       a = 5
       b = a * 5
       op:
@@ -34,9 +34,9 @@ suite "[when-let]":
         res = "y as float / 10.0 = " & $(test(y) / 10.0)
     check res == "y as float / 10.0 = 12.5"
 
-  test "[when-let] false by bool":
+  test "[if-let] false by bool":
     var res = ""
-    when_let:
+    if_let:
       a = 5
       b = a * 5
       c = false
@@ -44,11 +44,11 @@ suite "[when-let]":
         res = "Hallo! " & $a & " and " & $b
     check res == ""
 
-  test "[when-let] false from proc call":
+  test "[if-let] false from proc call":
     proc callFalse(x: int): bool =
       result = if x < 10: false else: true
     var res = ""
-    when_let:
+    if_let:
       a = 5
       b = a * 5
       c = callFalse(a)
@@ -56,9 +56,9 @@ suite "[when-let]":
         res = "a is smaller 10!"
     check res == ""
 
-  test "[when-let] assigning a seq":
+  test "[if-let] assigning a seq":
     var res: seq[int] = @[]
-    when_let:
+    if_let:
       a = @[1, 2, 3]
       b = 5
       op:
@@ -66,36 +66,58 @@ suite "[when-let]":
           res.add x * b
     check res == @[5, 10, 15]
 
-  test "[when-let] false from emtpy seq":
+  test "[if-let] false from emtpy seq":
     proc emptySeq(x: int): seq[int] =
       for i in 0 ..< x:
         result.add i
     var res = ""
-    when_let:
+    if_let:
       a = 0
       b = emptySeq(a)
       op:
         res = "Seq is empty!"
     check res == ""
 
-  test "[when-let] non nil ref object":
+  test "[if-let] non nil ref object":
     var obj = new int
     obj[] = 5
     var res = 0
-    when_let:
+    if_let:
       a = obj
       b = 5
       op:
         res = obj[] + b
     check res == 10
 
-  test "[when-let] false due to nil pointer":
+  test "[if-let] false due to nil pointer":
     var somePtr: pointer = nil
     var res = ""
-    when_let:
+    if_let:
       a = 5
       b = a * 5
       c = somePtr
       op:
         res = "Pointer is nil: " & $(c.isNil)
     check res == ""
+
+  test "[if-let] assignment with else":
+    var res = ""
+    if_let:
+      a = 5
+      b = true
+      op:
+        res = "Output: " & $a
+      elsedo:
+        res = "It was false!"
+    check res == "Output: 5"
+
+  test "[if-let] false assignment with else":
+    var res = ""
+    if_let:
+      a = 5
+      b = false
+      op:
+        res = "Output: " & $a
+      elsedo:
+        res = "It was false!"
+    check res == "It was false!"
